3bdc8049e367e2f5d33dd7aa4fe4b11875907c5f
Thomas <tsdexter@gmail.com>
2019-04-15 17:35:48 -0400

    end video 1

--- WeatherViewer/AppDelegate.swift
+++ WeatherViewer/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
--- WeatherViewer/ViewController.swift
+++ WeatherViewer/ViewController.swift
@@ -12,9 +12,61 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(headerLabel)
+        view.addSubview(searchButton)
+        view.addSubview(searchField)
+        view.setNeedsUpdateConstraints()
     }
 
-
+    lazy var headerLabel: UILabel! = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.text = "Weather Viewer"
+        view.font = view.font.withSize(36)
+        view.textAlignment = .center
+        
+        return view
+    }()
+    
+    lazy var searchField: UITextField! = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.placeholder = "City"
+        view.textAlignment = .left
+        view.font = .systemFont(ofSize: 24)
+        
+        return view
+    }()
+    
+    lazy var searchButton: UIButton! = {
+        let view = UIButton()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.setTitle("Search", for: .normal)
+        view.backgroundColor = UIColor.white
+        view.setTitleColor(UIColor.black, for: .normal)
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.black.cgColor
+        
+        return view
+    }()
+    
+    override func updateViewConstraints() {
+        let margins = view.layoutMarginsGuide
+        
+        headerLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: 15).isActive = true
+        headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        searchField.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20).isActive = true
+        searchField.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 5).isActive = true
+        
+        searchButton.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 20).isActive = true
+        searchButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 5).isActive = true
+        
+        super.updateViewConstraints()
+    }
 }
 
