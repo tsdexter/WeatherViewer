54923d8ccd5d58405a8d4105d5a85898fe3b5e65
Thomas <tsdexter@gmail.com>
2019-04-15 17:03:42 -0400

    Initial Commit

---
+++ WeatherViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherViewer
+//
+//  Created by Thomas Dexter on 2019-04-15.
+//  Copyright © 2019 Thomas Dexter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
